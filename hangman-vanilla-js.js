6896e4b886efd9296cd975cac1a6345aaa8e2c34
rayacooper <47796461+rayacooper@users.noreply.github.com>
2020-10-01 18:42:08 -0600

    Winning works. Need to add hangman & lose state

--- hangman-vanilla-js.js
+++ hangman-vanilla-js.js
@@ -1,17 +1,21 @@
 const inputText = document.getElementById('Text');
 const placeHolder = document.getElementById('placeHolder')
 const guessWrong = document.getElementById('guessWrong')
+const WordPrompt = document.getElementById('WordPrompt')
 
 const allThemLetters = a = 'a', b = 'b', c = 'c', d = 'd', e = 'e', f = 'f', g = 'g', h = 'h', i = 'i', j = 'j', k = 'k',
     l = 'l', m = 'm', n = 'n', o = 'o', p = 'p', q = 'q', r = 'r', s = 's', t = 't', u = 'u', v = 'v', w = 'w', x = 'x',
     y = 'y', z = 'z'
-const listOfWords = ['syntax', 'code', 'tilde', 'error', 'test', 'buffer']
+const listOfWords = ['syntax', 'coding', 'tilde', 'error', 'test', 'buffer']
+const listOfPrompts = ["This has to be right, or the computer can't read your code!", "Everybody's favorite hobby!", "The little squiggle key", "When you mess up your code, you get an...", "Do this to make sure your code works!", "C'mon little grey bar, keep going!"]
 
+let rando = 0;
 let chances = 0;
 let lettersCorrect = 0
 let rightGuesses = []
 let wrongGuesses = []
 let secretWord = []
+let prompter = ''
 let tempString = ''
 
 // const testing = () => {
@@ -22,37 +26,43 @@ const newGame = () => {
     lettersCorrect = 0;
     rightGuesses = [];
     wrongGuesses = [];
-    secretWord = listOfWords[Math.floor(Math.random() * 5)].split('');
+    rando = Math.floor(Math.random() * 5)
+    secretWord = listOfWords[rando].split('');
+    prompter = listOfPrompts[rando];
     tempString = secretWord.map(e => e = '_').join('');
     placeHolder.innerHTML = tempString;
-    guessWrong.innerHTML = wrongGuesses.join('')
+    WordPrompt.innerHTML = prompter;
+    guessWrong.innerHTML = wrongGuesses.join('');
     console.log(secretWord)
 }
 
 const letterGuess = (x) => {
-    chances++
     if(secretWord.includes(x) && chances < 5){
-        rightGuesses.push(x)
-        alert('Yay')
+        rightGuesses.push(x);
+        updatePlaceholder(x);
+        if(tempString === secretWord.join('')){
+            setTimeout(() => {
+                alert('Good job, you win!')
+            }, 500);
+        }
     }else if(!secretWord.includes(x) && chances < 5){
+        chances++
         wrongGuesses.push(x)
         guessWrong.innerHTML = wrongGuesses.join('')
         alert('Whoops! Try again!')
     }else if(chances >= 5){
         alert('Sorry, out of guesses!')
     }
-    console.log()
 }
 
-const guessing = () => {
-    if (inputText.value.toLowerCase() == secretWord){
-        alert("You Win!!!")
-    }else if (chances < 5){
-        alert("Sorry, try again!")
-        chances++
-    }else{
-        alert("Sorry, you lose!")
-    }
-
+const updatePlaceholder = (x) => {
+    tempString = tempString.split('')
+        for (var i = 0; i < secretWord.length; i++){ // Adding the 'var' here allowed me to do this for loop
+            if (secretWord[i] === x){
+                tempString[i] = x;
+            }
+        }
+    tempString = tempString.join('')
+    placeHolder.innerHTML = tempString
 }
 
